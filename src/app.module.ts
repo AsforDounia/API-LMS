dd50d315af8fbd58afe776f98514fa533ea77229
samira kibous <kiboussamira71@gmail.com>
2026-01-07 11:59:29 +0100

    AL-122 quiz and question controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { QuizModule } from './quiz/quiz.module'; 
+import { QuestionModule } from './question/question.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/lms'), 
+    QuizModule,
+    QuestionModule
+
+  ],
   controllers: [AppController],
   providers: [AppService],
+
 })
 export class AppModule {}
