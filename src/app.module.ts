19a10e890d672d76560e724bacb896b52aabdd6a
samira kibous <kiboussamira71@gmail.com>
2026-01-07 12:12:09 +0100

    pulling from preprod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { QuizModule } from './quiz/quiz.module'; 
-import { QuestionModule } from './question/question.module';
-
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './users/entities/user.entity';
 import { ConfigModule } from '@nestjs/config';
+import { QuizModule } from './quiz/quiz.module';
+import { QuestionModule } from './question/question.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || ''),
+    UsersModule,
+    AuthModule,
+    QuizModule,
+    QuestionModule
+  ],
   controllers: [AppController],
   providers: [AppService],
-
 })
 export class AppModule {}
