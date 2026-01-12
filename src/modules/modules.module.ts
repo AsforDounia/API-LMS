dd5210f0d9498ff24a52db429f5e582f89544e1d
samira kibous <kiboussamira71@gmail.com>
2026-01-12 17:21:01 +0100

    Add ModuleProgress schema to Mongoose module imports

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -4,13 +4,15 @@ import { ModulesController } from './modules.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ModuleSchema } from './entities/module.entity';
 import { CourseSchema } from '@src/courses/entities/course.entity';
+import { ModuleProgress, ModuleProgressSchema } from '../module-progress/entities/module-progress.entity';
 
 @Module({
   imports: [
-      MongooseModule.forFeature([
-        { name: Module.name, schema: ModuleSchema },
-        { name: 'Course', schema: CourseSchema },
-      ]),
+    MongooseModule.forFeature([
+      { name: Module.name, schema: ModuleSchema },
+      { name: 'Course', schema: CourseSchema },
+      { name: ModuleProgress.name, schema: ModuleProgressSchema },
+    ]),
   ],
   controllers: [ModulesController],
   providers: [ModulesService],
